25b6d9488e49bc8443d4a9f4fe9e3e271475f9aa
NIck Marucci <npm1514@gmail.com>
2020-11-01 09:53:08 -0700

    add hours

--- src/components/Map.js
+++ src/components/Map.js
@@ -11,6 +11,7 @@ class MapComponent extends Component {
           <Hex className="desktop-hex" color={green} top={-77} left={-67} size={200}/>
           <Hex className="mobile-hex" color={pink} top={-77} left={-27} size={75}/>
           <h2>Come grab a cup of coffee at 29<sub>E</sub> 400<sub>S</sub>!</h2>
+          <h2>Hours | 8am - 8pm</h2>
           <iframe
             className="desktop-map"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.0500648091283!2d-111.89217568459362!3d40.760923579326665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8752f51053dac723%3A0x39f2100c58807a0b!2s29%20E%20400%20S%2C%20Salt%20Lake%20City%2C%20UT%2084111!5e0!3m2!1sen!2sus!4v1600403552691!5m2!1sen!2sus"
