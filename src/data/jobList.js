446677e164175d1ccd8a3da1a09fe602f8478ac2
NIck Marucci <npm1514@gmail.com>
2023-01-07 18:15:54 -0700

    change jobs

--- src/data/jobList.js
+++ src/data/jobList.js
@@ -1,14 +1,14 @@
 module.exports = [
-  {
-    id: "parttimebarista",
-    title: "Part Time Barista",
-    description: "Here at Cafe Juniper our biggest priority is creating a positive, comfortable work environment. We encourage our employees to express themselves however they feel, and voice their opinion whenever they feel necessary. Crafting specialty coffee, making delicious food, and baking small treats are just a couple of things you can expect when joining our team. Starting wage is $8.50 plus tips which averages to be about $12 - $14 an hour. Shifts range from 4 to 6 hours, with 10 - 20 hours a week with room in expanding those hours. Looking forward to having you join our team!"
-  },
   // {
-  //   id: "b2bsales",
-  //   title: "B2B Sales",
-  //   description: "We are looking for a contract sales person. In this role, the sales person will reach out to local businesses and make efforts in setting up repeat delivery sales for daily or weekly coffee delivery and pickup. This will be a by-commission position earning 20% on all contracts signed off by a local business. This is an important role in the future growth of our business and has opportunity to become a salary role or partner role down the road."
+  //   id: "parttimebarista",
+  //   title: "Part Time Barista",
+  //   description: "Here at Cafe Juniper our biggest priority is creating a positive, comfortable work environment. We encourage our employees to express themselves however they feel, and voice their opinion whenever they feel necessary. Crafting specialty coffee, making delicious food, and baking small treats are just a couple of things you can expect when joining our team. Starting wage is $8.50 plus tips which averages to be about $12 - $14 an hour. Shifts range from 4 to 6 hours, with 10 - 20 hours a week with room in expanding those hours. Looking forward to having you join our team!"
   // },
+  {
+    id: "b2bsales",
+    title: "B2B Sales",
+    description: "We are looking for a contract sales person. In this role, the sales person will reach out to local businesses and make efforts in setting up repeat delivery sales for daily or weekly coffee delivery and pickup. This will be a by-commission position earning 20% on all contracts signed off by a local business. This is an important role in the future growth of our business and has opportunity to become a salary role or partner role down the road."
+  },
   // {
   //   id: "driver",
   //   title: "Delivery/Pickup Driver",
